cce61eba2aabfa2758f9cbd3cbbd07a98fac5568
Eduard Vlasov <e.vlasov@realtimeboard.com>
2018-10-18 13:31:38 +0500

    Additional logging, state reporting after the flush

--- target_postgres/__init__.py
+++ target_postgres/__init__.py
@@ -35,92 +35,115 @@ def persist_lines(config, lines):
     headers = {}
     validators = {}
     csv_files_to_load = {}
-    row_count = {}
+    row_counts = {}
+    flushed_row_counts = {}
     stream_to_sync = {}
     primary_key_exists = {}
-    batch_size = config['batch_size'] if 'batch_size' in config else 100000
+    batch_size = config['batch_size'] if 'batch_size' in config else 10000
 
     now = datetime.now().strftime('%Y%m%dT%H%M%S')
 
     # Loop over lines from stdin
     for line in lines:
         try:
-            o = json.loads(line)
+            object = json.loads(line)
         except json.decoder.JSONDecodeError:
             logger.error("Unable to parse:\n{}".format(line))
             raise
 
-        if 'type' not in o:
+        if 'type' not in object:
             raise Exception("Line is missing required key 'type': {}".format(line))
-        t = o['type']
+        object_type = object['type']
 
-        if t == 'RECORD':
-            if 'stream' not in o:
-                raise Exception("Line is missing required key 'stream': {}".format(line))
-            if o['stream'] not in schemas:
-                raise Exception(
-                    "A record for stream {} was encountered before a corresponding schema".format(o['stream']))
-
+        if object_type == 'RECORD':
             # Get schema for this record's stream
-            stream = o['stream']
+            if 'stream' not in object:
+                raise Exception("Line is missing required key 'stream': {}".format(line))
+            stream = object['stream']
+
+            if stream not in schemas:
+                raise Exception(
+                    "A record for stream {} was encountered before a corresponding schema".format(object['stream']))
 
             # Validate record
-            validators[stream].validate(o['record'])
-
+            validators[stream].validate(object['record'])
             sync = stream_to_sync[stream]
 
-            primary_key_string = sync.record_primary_key_string(o['record'])
+            primary_key_string = sync.record_primary_key_string(object['record'])
             if stream not in primary_key_exists:
                 primary_key_exists[stream] = {}
-            if primary_key_string and primary_key_string in primary_key_exists[stream]:
-                flush_records(o, csv_files_to_load, row_count, primary_key_exists, sync)
 
-            csv_line = sync.record_to_csv_line(o['record'])
-            csv_files_to_load[o['stream']].write(bytes(csv_line + '\n', 'UTF-8'))
-            row_count[o['stream']] += 1
+            # flush prepared records before a new row with the PK existing in this bath
+            if primary_key_string and primary_key_string in primary_key_exists[stream]:
+                logger.debug("Encountered a PK value %s which exists in the bath, flushing", primary_key_string)
+                flush_records(stream, csv_files_to_load, row_counts, primary_key_exists,
+                              sync, state, flushed_row_counts)
+
+            # add the record to a temp file
+            csv_line = sync.record_to_csv_line(object['record'])
+            csv_files_to_load[stream].write(bytes(csv_line + '\n', 'UTF-8'))
+            row_counts[stream] += 1
+
             if primary_key_string:
                 primary_key_exists[stream][primary_key_string] = True
 
-            if row_count[o['stream']] >= batch_size:
-                flush_records(o, csv_files_to_load, row_count, primary_key_exists, sync)
+            # in case we reached the bath size limit
+            if row_counts[stream] >= batch_size:
+                flush_records(stream, csv_files_to_load, row_counts, primary_key_exists,
+                              sync, state, flushed_row_counts)
 
-            state = None
-        elif t == 'STATE':
-            logger.debug('Setting state to {}'.format(o['value']))
-            state = o['value']
-        elif t == 'SCHEMA':
-            if 'stream' not in o:
+            # state = None
+        elif object_type == 'STATE':
+            logger.debug('Setting state to {}'.format(object['value']))
+            state = object['value']
+
+        elif object_type == 'SCHEMA':
+            if 'stream' not in object:
                 raise Exception("Line is missing required key 'stream': {}".format(line))
-            stream = o['stream']
-            schemas[stream] = o
-            validators[stream] = Draft4Validator(o['schema'])
-            if 'key_properties' not in o:
+            stream = object['stream']
+
+            schemas[stream] = object
+            validators[stream] = Draft4Validator(object['schema'])
+            if 'key_properties' not in object:
                 raise Exception("key_properties field is required")
-            key_properties[stream] = o['key_properties']
-            stream_to_sync[stream] = DbSync(config, o)
+            key_properties[stream] = object['key_properties']
+            stream_to_sync[stream] = DbSync(config, object)
             stream_to_sync[stream].create_schema_if_not_exists()
             stream_to_sync[stream].sync_table()
-            row_count[stream] = 0
+            row_counts[stream] = 0
+            flushed_row_counts[stream] = 0
             csv_files_to_load[stream] = TemporaryFile(mode='w+b')
-        elif t == 'ACTIVATE_VERSION':
+
+        elif object_type == 'ACTIVATE_VERSION':
             logger.debug('ACTIVATE_VERSION message')
+
         else:
             raise Exception("Unknown message type {} in message {}"
-                            .format(o['type'], o))
+                            .format(object['type'], object))
 
-    for (stream_name, count) in row_count.items():
+    logger.info("Finished reading input, flushing remaining rows")
+    for (stream_name, count) in row_counts.items():
         if count > 0:
-            stream_to_sync[stream_name].load_csv(csv_files_to_load[stream_name], count)
+            flush_records(stream_name, csv_files_to_load, row_counts, primary_key_exists,
+                          stream_to_sync[stream_name], state, flushed_row_counts)
+
+    logger.info("Processed records:")
+    for (stream_name, count) in flushed_row_counts.items():
+        logger.info("%s - %d", stream_name, count)
 
     return state
 
 
-def flush_records(o, csv_files_to_load, row_count, primary_key_exists, sync):
-    stream = o['stream']
-    sync.load_csv(csv_files_to_load[stream], row_count[stream])
-    row_count[stream] = 0
+def flush_records(stream, csv_files_to_load, row_counts, primary_key_exists, sync, state, flushed_row_counts):
+    stream_row_count = row_counts[stream]
+    sync.load_csv(csv_files_to_load[stream], stream_row_count)
+    row_counts[stream] = 0
+    flushed_row_counts[stream] += stream_row_count
+
     primary_key_exists[stream] = {}
     csv_files_to_load[stream] = TemporaryFile(mode='w+b')
+    logger.info("Flushed %d row to the stream %s", stream_row_count, stream)
+    emit_state(state)
 
 
 def main():
